0835146e10e414ad62ab959f6c23a07acce2bd70
Rahul Derashri <rahul.derashri@gmail.com>
2015-11-07 14:06:23 -0500

    MCL Algorithm Commit

---
+++ src/MCLAlgorithm/TransitionMatrix.java
@@ -0,0 +1,28 @@
+package MCLAlgorithm;
+
+
+
+/**
+ * This class generates Transition matrix from the input data 
+ */
+
+public class TransitionMatrix {
+	static double[][] generateTransitionMatrix(int[][] adjMatrix){
+		double[][] matrix = new double[adjMatrix.length][adjMatrix.length];
+		
+		for(int j=0;j < matrix[0].length;j++){
+			int count = 0;	// counts the number of ones on each column
+			for(int i=0;i < matrix.length;i++){
+				if(adjMatrix[i][j] == 1) count++;
+			}
+			
+			
+			for(int i=0;i<matrix.length;i++){
+				if(adjMatrix[i][j] == 1) {
+					matrix[i][j] = (double)1/count;
+				}
+			}
+		}
+		return matrix;
+	}
+}
